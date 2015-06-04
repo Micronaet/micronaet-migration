291f5ad10175a24a7176a9dacb82dd6a85a7ea5d
nicothebrush <nicola.riolini@gmail.com>
2015-06-04 15:41:49 +0200

    Add xml view in quotation_photo

--- quotation_photo/README.md
+++ quotation_photo/README.md
@@ -2,5 +2,6 @@ Quotation Photo
 ===============
 
 Add a picture for product load element from files images
+*Insert photo field from file in Quotation*
 
 Migration of *quotation_photo_fiam*
--- quotation_photo/__openerp__.py
+++ quotation_photo/__openerp__.py
@@ -1,38 +1,29 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
+# -*- coding: utf-8 -*-
+###############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    $Id$
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# Copyright (c) 2008-2010 SIA "KN dati". (http://kndati.lv) All Rights Reserved.
-#                    General contacts <info@kndati.lv>
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
 #
-# Copyright (c) 2010-2011"Micronaet s.r.l.". (http://www.micronaet.it) 
-#                    All Rights Reserved.
-#                    General contacts <info@micronaet.it>
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+###############################################################################
 
 {
     'name': 'Quotation photo',
     'version': '0.0.1',
     'category': 'Generic Modules/Customization',
-    'description': """ 
-        Insert photo field from file in Quotation
-        """,
     'author': 'Micronaet s.r.l.',
     'website': 'http://www.micronaet.it',
     'depends': [
@@ -41,9 +32,9 @@
         'sale',
         ],
     'init_xml': [], 
-    'update_xml': [
+    'data': [
         'security/ir.model.access.csv',
-        #'photo_views.xml',
+        'photo_views.xml',
         ],
     'demo_xml': [],
     'active': False, 
