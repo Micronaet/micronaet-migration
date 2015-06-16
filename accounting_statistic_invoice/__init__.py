e48f90897a4f0e6dc6e852d022469bfb0be6dfd1
nicothebrush <nicola.riolini@gmail.com>
2015-06-16 12:12:20 +0200

    [ADD] Start add module for pack the statistic elements

--- accounting_statistic_invoice/__init__.py
+++ accounting_statistic_invoice/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# ODOO (ex OpenERP)
+# ODOO (ex OpenERP) 
 # Open Source Management Solution
 # Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -12,37 +12,14 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-{
-    'name': 'Mexal order statistic',
-    'version': '0.1',
-    'category': 'Statistic',
-    'author': 'Micronaet s.r.l.',
-    'website': 'http://www.micronaet.it',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-        'sale',
-        'base_accounting_program',
-        'report_aeroo',
-        #'todo_list',
-        ],
-    'init_xml': [],
-    'demo_xml': [],
-    'data': [
-        'security/order.xml',
-        'security/ir.model.access.csv',
-        #'order.xml', # TODO rewrite statistic
-        'menuitem.xml',
-        'report/report_order.xml',
-        ],
-    'active': False,
-    'installable': True,
-    }
+
+from . import invoice
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
