ccc19d3b4adbb406a0e73f4056186b3b9870d2c3
nicothebrush <nicola.riolini@gmail.com>
2015-07-31 13:03:46 +0200

    Remove temp files

--- label_easy/wizard/csv/temp/purchase.cmd
+++
@@ -1,9 +0,0 @@
-formatname="c:\wineasy\openerp\purchase\Purchase"
-formatcount=10
-testprint=off
-TEXT36="IIRIPOFE-BI--S"
-TEXT42="1/1"
-useprinter=1
-jobdescription="Order: PO00090 (IIRIPOFE-BI--S Part: 1/1)"
-singlejob=on
-;
