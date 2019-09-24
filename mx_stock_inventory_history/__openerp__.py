3ae60c52b98e56a12f885a25a0c47800c91a1f30
nicothebrush <nicola.riolini@gmail.com>
2019-09-24 16:10:10 +0200

    [ADD] mx_stock_inventory_history: manage previous year start date

---
+++ mx_stock_inventory_history/__openerp__.py
@@ -0,0 +1,42 @@
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'History start status',
+    'version': '0.1',
+    'category': 'Inventory',
+    'description': '''    
+        Save start status (every year was rewrited)
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'inventory_status',
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        'inventory_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
---
+++ mx_stock_inventory_history/history.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+class ProductProductStartHistory(orm.Model):
+    """ Model name: ProductProductStartHistory
+    """
+    
+    _name = 'product.product.start.history'
+    _description = 'Start history'
+    _rec_name = 'product_id'
+    
+    def history_start_elements(self, cr, uid, mx_start_date=False, 
+            context=None):
+        ''' Save current history
+        '''
+        if not date:
+            _logger.error('Cannoy history without start date!')
+            continue
+        
+        # Remove previous history values:
+        product_pool = self.pool.get('product.product')
+        product_ids = product_pool.search(cr, uid, [
+            ('mx_start_date', '=', mx_start_date),
+            ], context=context)
+        _logger.warning('Delete previous record: %s' % len(product_ids))
+        product_pool.unlink(cr, uid, product_ids, context=context)
+
+        # Update with current:
+        product_ids = product_pool.search(cr, uid, [], context=context)
+        total = len(product_ids)
+        i = 0
+        for product in product_pool.browse(
+                cr, uid, product_ids, context=context):
+            i += 1
+            if i % 100 == 0:    
+                _logger.warning('Updated %s of %s' % (
+                    i, total,
+                    ))    
+
+            self.create(cr, uid, {
+                'product_id': product.id,
+                'mx_start_date': product.mx_start_date,
+                'mx_start_qty': product.mx_start_qty,
+                }, context=context)    
+            
+    
+    _columns = {
+        'product_id': fields.many2one(
+            'product.product', 'Product', required=True),
+            
+        'mx_start_date': fields.date('Start date'),
+        'mx_start_qty': fields.float('Inventory start qty', 
+            digits=(16, 2), # TODO parametrize
+            help='Inventory at 1/1 for current year'),
+    }
+    
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
