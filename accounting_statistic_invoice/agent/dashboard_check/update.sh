e61a2b47703d9b1a1f491b23f5f67d92a3527a5a
nicothebrush <nicola.riolini@gmail.com>
2023-06-08 14:50:29 +0200

    generic bugfix (fast message)

--- accounting_statistic_invoice/agent/dashboard_check/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -avh -ssh administrator@192.168.1.181:/home/administrator/photo/log/dashboard/statistic.partner.FIA.* ./data
-# scp administrator@192.168.1.181:/home/administrator/photo/log/statistic.partner.FIA.* ./data/
-# sftp://administrator@192.168.1.181/home/administrator/photo/log/dashboard
