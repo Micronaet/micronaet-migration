0f11b33f12bb721f0e81156ff5087c5a28083d70
nicothebrush <nicola.riolini@gmail.com>
2023-06-08 17:18:03 +0200

    generic bugfix (fast message)

--- accounting_statistic_invoice/agent/dashboard_check/2 controllo_dashboard_ordinato.py
+++ accounting_statistic_invoice/agent/dashboard_check/2 controllo_dashboard_ordinato.py
@@ -33,7 +33,7 @@ elif len(parameter) == 1:  # Path and use last 2:
     path = parameter[0]
     for root, folders, files in os.walk(path):
         files = sorted(
-            [os.join(root, f) for f in files if f.startswith(start)])[-2]
+            [os.path.join(root, f) for f in files if f.startswith(start)])[-2]
         break
 else:
     print('No 2 files passed and not path passed!: %s' % (parameter, ))
